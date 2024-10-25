e8ec9614fae9c76c38c802757005f3cfb514f19a
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-10-25 14:42:19 +0000

    no.0 fix

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,3 +9,12 @@ def index_range(page, page_size):
     start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
+
+
+if __name__ == "__main__":
+    res = index_range(1, 7)
+    print(type(res))
+    print(res)
+    res = index_range(page=3, page_size=15)
+    print(type(res))
+    print(res)
