e01d8c1f8502b5b11023e8d620b0f6e12b0c44e7
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-10-30 21:26:40 +0000

    task no.4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+""" MRU Caching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRUCache class
+    """
+
+    def __init__(self):
+        """ Constructor
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key not in self.cache_data:
+                    discard = self.queue[-1]
+                    del self.cache_data[discard]
+                    self.queue.pop(-1)
+                    print("DISCARD:", discard)
+            self.cache_data[key] = item
+            if key in self.queue:
+                self.queue.remove(key)
+            self.queue.append(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.cache_data[key]
+        return None
+
+
+if __name__ == "__main__":
+    my_cache = MRUCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+    print(my_cache.get("B"))
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
+    my_cache.put("G", "San Francisco")
+    my_cache.print_cache()
+    my_cache.put("H", "H")
+    my_cache.print_cache()
+    my_cache.put("I", "I")
+    my_cache.print_cache()
+    my_cache.put("J", "J")
+    my_cache.print_cache()
+    my_cache.put("K", "K")
+    my_cache.print_cache()
