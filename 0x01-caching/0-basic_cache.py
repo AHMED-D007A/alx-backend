be92087e00b3d1155fa565e897ed9578af2729c5
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-10-30 21:03:42 +0000

    test

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-""" Basic dictionary """
+"""
+Basic Cache dictionary
+for the basic cache class
+"""
 
 
 class BaseCaching():
@@ -35,11 +38,13 @@ class BaseCaching():
 
 
 class BasicCache(BaseCaching):
-    """ BasicCache inherits from BaseCaching
+    """
+    BasicCache inherits from BaseCaching
     """
 
     def put(self, key, item):
-        """ Add an item in the cache
+        """
+        Add an item in the cache
         """
         if key is None or item is None:
             return
@@ -49,27 +54,28 @@ class BasicCache(BaseCaching):
             del self.cache_data[keys[0]]
 
     def get(self, key):
-        """ Get an item by key
+        """
+        Get an item by key
         """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
 
 
-if __name__ == "__main__":
-    my_cache = BasicCache()
-    my_cache.print_cache()
-    my_cache.put("A", "Hello")
-    my_cache.put("B", "World")
-    my_cache.put("C", "Holberton")
-    my_cache.print_cache()
-    print(my_cache.get("A"))
-    print(my_cache.get("B"))
-    print(my_cache.get("C"))
-    print(my_cache.get("D"))
-    my_cache.print_cache()
-    my_cache.put("D", "School")
-    my_cache.put("E", "Battery")
-    my_cache.put("A", "Street")
-    my_cache.print_cache()
-    print(my_cache.get("A"))
+# if __name__ == "__main__":
+#     my_cache = BasicCache()
+#     my_cache.print_cache()
+#     my_cache.put("A", "Hello")
+#     my_cache.put("B", "World")
+#     my_cache.put("C", "Holberton")
+#     my_cache.print_cache()
+#     print(my_cache.get("A"))
+#     print(my_cache.get("B"))
+#     print(my_cache.get("C"))
+#     print(my_cache.get("D"))
+#     my_cache.print_cache()
+#     my_cache.put("D", "School")
+#     my_cache.put("E", "Battery")
+#     my_cache.put("A", "Street")
+#     my_cache.print_cache()
+#     print(my_cache.get("A"))
