9aabd7d07b0931fe1cd761502102bb2079598559
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-10-30 21:28:19 +0000

    done

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+""" LFU Caching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ LFUCache class
+    """
+
+    def __init__(self):
+        """ Constructor
+        """
+        super().__init__()
+        self.queue = []
+        self.count = {}
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key not in self.cache_data:
+                    discard = self.queue.pop(0)
+                    del self.cache_data[discard]
+                    del self.count[discard]
+                    print("DISCARD:", discard)
+            self.cache_data[key] = item
+            if key in self.queue:
+                self.queue.remove(key)
+            self.queue.append(key)
+            if key in self.count:
+                self.count[key] += 1
+            else:
+                self.count[key] = 1
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            self.queue.remove(key)
+            self.queue.append(key)
+            self.count[key] += 1
+            return self.cache_data[key]
+        return None
+
+    def get_count(self):
+        """ Get count dictionary
+        """
+        return self.count
+
+
+if __name__ == "__main__":
+    my_cache = LFUCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+    print(my_cache.get("B"))
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
+    my_cache.put("G", "San Francisco")
+    my_cache.print_cache()
+    my_cache.put("H", "H")
+    my_cache.print_cache()
+    my_cache.put("I", "I")
+    my_cache.print_cache()
+    print(my_cache.get("I"))
+    print(my_cache.get("H"))
+    print(my_cache.get("I"))
+    print(my_cache.get("H"))
+    print(my_cache.get("I"))
+    print(my_cache.get("H"))
+    my_cache.put("J", "J")
+    my_cache.print_cache()
+    my_cache.put("K", "K")
+    my_cache.print_cache()
+    my_cache.put("L", "L")
+    my_cache.print_cache()
+    my_cache.put("M", "M")
+    my_cache.print_cache()
