d9995cb0440e9fb3c812d1a8bc787256e6b2d5b8
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-10-30 20:58:49 +0000

    task no.0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+""" Basic dictionary """
+
+
+class BaseCaching():
+    """ BaseCaching defines:
+      - constants of your caching system
+      - where your data are stored (in a dictionary)
+    """
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        self.cache_data = {}
+
+    def print_cache(self):
+        """ Print the cache
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print("{}: {}".format(key, self.cache_data.get(key)))
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        raise NotImplementedError("put must be \
+                                  implemented in your cache class")
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        raise NotImplementedError("get must be \
+                                  implemented in your cache class")
+
+
+class BasicCache(BaseCaching):
+    """ BasicCache inherits from BaseCaching
+    """
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            keys = list(self.cache_data.keys())
+            del self.cache_data[keys[0]]
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
+
+
+if __name__ == "__main__":
+    my_cache = BasicCache()
+    my_cache.print_cache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    print(my_cache.get("D"))
+    my_cache.print_cache()
+    my_cache.put("D", "School")
+    my_cache.put("E", "Battery")
+    my_cache.put("A", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
