40bcd18712af1dc72b3b4a9777b52cf4eee4e8f0
AHMED-D007A <ahmed.mamdouh8840@gmail.com>
2024-10-30 21:14:05 +0000

    test no.0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -18,9 +18,9 @@ class BasicCache(BaseCaching):
         if key is None or item is None:
             return
         self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            keys = list(self.cache_data.keys())
-            del self.cache_data[keys[0]]
+        # if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+        #     keys = list(self.cache_data.keys())
+        #     del self.cache_data[keys[0]]
 
     def get(self, key):
         """
@@ -31,20 +31,20 @@ class BasicCache(BaseCaching):
         return self.cache_data[key]
 
 
-# if __name__ == "__main__":
-#     my_cache = BasicCache()
-#     my_cache.print_cache()
-#     my_cache.put("A", "Hello")
-#     my_cache.put("B", "World")
-#     my_cache.put("C", "Holberton")
-#     my_cache.print_cache()
-#     print(my_cache.get("A"))
-#     print(my_cache.get("B"))
-#     print(my_cache.get("C"))
-#     print(my_cache.get("D"))
-#     my_cache.print_cache()
-#     my_cache.put("D", "School")
-#     my_cache.put("E", "Battery")
-#     my_cache.put("A", "Street")
-#     my_cache.print_cache()
-#     print(my_cache.get("A"))
+if __name__ == "__main__":
+    my_cache = BasicCache()
+    my_cache.print_cache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
+    print(my_cache.get("B"))
+    print(my_cache.get("C"))
+    print(my_cache.get("D"))
+    my_cache.print_cache()
+    my_cache.put("D", "School")
+    my_cache.put("E", "Battery")
+    my_cache.put("A", "Street")
+    my_cache.print_cache()
+    print(my_cache.get("A"))
